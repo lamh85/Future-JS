b80af47241082ce4c7e4b65cd4bfa60ee9df2e22
Hon Lam <lamh85@gmail.com>
2016-05-15 20:24:55 -0700

    Added new methods for defining objects.

--- objects.js
+++ objects.js
@@ -1,3 +1,22 @@
 var x = 5;
 var y = 3;
 var myObject = {x, y}
+
+var computedObjectName = {
+  [x + y]: "foo"
+}
+
+var object1 = {
+  hello: "world",
+  foo: "bar"
+}
+
+var object2 = {
+  foo: "another",
+  wrist: "watch"
+}
+
+var object3 = {
+  ...object1,
+  ...object2
+}
